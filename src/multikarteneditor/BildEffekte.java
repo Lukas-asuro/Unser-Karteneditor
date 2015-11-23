037f0acc1b3315c9371366f5ebf5ddf7e6399c0a
Lukas <Lukas@Lukas-HP>
2015-11-23 21:13:19 +0100

    New jFrame: BildEffekte 2

--- src/multikarteneditor/BildEffekte.java
+++ src/multikarteneditor/BildEffekte.java
@@ -34,6 +34,7 @@ public class BildEffekte extends javax.swing.JFrame {
     int d1 = 0;
     int d2 = 0;
     int d3 = 0;
+    int filter=0;
 
     /**
      * Creates new form BildEffekte
@@ -68,6 +69,9 @@ public class BildEffekte extends javax.swing.JFrame {
         };
         jLabel1 = new javax.swing.JLabel();
         jSlider1 = new javax.swing.JSlider();
+        jButton3 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -109,11 +113,18 @@ public class BildEffekte extends javax.swing.JFrame {
             .addGap(0, 0, Short.MAX_VALUE)
         );
 
-        jSlider1.setMaximum(500);
-        jSlider1.setValue(500);
+        jSlider1.setMaximum(200);
+        jSlider1.setValue(0);
         jSlider1.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 jSlider1StateChanged(evt);
+            }
+        });
+
+        jButton3.setText("Filter verwenden");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
             }
         });
 
@@ -122,9 +133,9 @@ public class BildEffekte extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -139,11 +150,17 @@ public class BildEffekte extends javax.swing.JFrame {
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addComponent(jLabel1)))
+                            .addComponent(jLabel1))
+                        .addGap(0, 50, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(20, 20, 20)
-                        .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(60, Short.MAX_VALUE))
+                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(49, 49, 49)
+                        .addComponent(jButton3)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -160,7 +177,12 @@ public class BildEffekte extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(45, 45, 45)
-                .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jSlider1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jButton3)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(28, 28, 28))
         );
 
@@ -194,10 +216,27 @@ public class BildEffekte extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jSlider1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSlider1StateChanged
+         filter = jSlider1.getValue();
+         jLabel3.setText(""+filter+"");
+    }//GEN-LAST:event_jSlider1StateChanged
+
+    private void jComboBox1ItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jComboBox1ItemStateChanged
+        int auswahl=jComboBox1.getSelectedIndex();
+        if(auswahl==1){
+            rof=255;
+            grf=255;
+            blf=255;
+            jLabel2.setText("Weiß");
+        }
+    }//GEN-LAST:event_jComboBox1ItemStateChanged
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+       if(bildoriginal!=null){
         Graphics2D g_b = bildoriginal.createGraphics();
         Graphics2D g_b2 = bildbearbeitet.createGraphics();
-        int filter = jSlider1.getValue();
+        
         //Ausstanzen
+        
         for (int i = 0; i < (jPanel1.getHeight() * jPanel1.getWidth()); i++) {
             int co = bildoriginal.getRGB(x, y);
             Color c = new Color(co);
@@ -210,27 +249,27 @@ public class BildEffekte extends javax.swing.JFrame {
             df = (int)Math.sqrt((d1 * d1) + (d2 * d2) + (d3 * d3)); //"Abstand" zur Filterfarbe berechen
             
             if (df <= filter) {
-                
-                g_b2.setColor(Color.ORANGE);
+               g_b2.setColor(Color.RED);
                g_b2.drawLine(x, y, 1, 1);
+                jPanel2.repaint();
             }
-            if ((x-1) == jPanel1.getWidth()) {
+            else{
+               g_b2.setColor(Color.GREEN);
+               g_b2.drawLine(x, y, 1, 1); //andere als Filterfarbe
+                jPanel2.repaint();
+            }
+            x++;
+            if (x == 186) {
                 x = 0;
                 y++;
             }
-x++;
-        }
-        jPanel2.repaint();
-    }//GEN-LAST:event_jSlider1StateChanged
 
-    private void jComboBox1ItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jComboBox1ItemStateChanged
-        int auswahl=jComboBox1.getSelectedIndex();
-        if(auswahl==1){
-            rof=255;
-            grf=255;
-            blf=255;
+        System.out.print(""+x+", ");
+        System.out.println(y);
         }
-    }//GEN-LAST:event_jComboBox1ItemStateChanged
+       
+       }
+    }//GEN-LAST:event_jButton3ActionPerformed
 
                     /**
                      * @param args the command line arguments
@@ -272,7 +311,7 @@ x++;
             int w = jPanel2.getWidth();
             int hoeheneu = bildbearbeitet.getHeight() * w / bildbearbeitet.getWidth();
             System.out.println("Maße des Bildes original: " + bildbearbeitet.getWidth() + " " + bildbearbeitet.getHeight() + " , nach Anpassung: " + w + " " + hoeheneu);
-
+            
             g.drawImage(bildbearbeitet, 0, 0, w, hoeheneu, this);
         } else {
             g.setColor(new Color(10, 10, 100));
@@ -284,8 +323,9 @@ x++;
             int w = jPanel1.getWidth();
             int hoeheneu = bildoriginal.getHeight() * w / bildoriginal.getWidth();
             System.out.println("Maße des Bildes original: " + bildoriginal.getWidth() + " " + bildoriginal.getHeight() + " , nach Anpassung: " + w + " " + hoeheneu);
-
+            
             g.drawImage(bildoriginal, 0, 0, w, hoeheneu, this);
+           
         } else {
             g.setColor(new Color(10, 10, 100));
             g.fillRect(0, 0, jPanel1.getWidth(), jPanel1.getHeight());
@@ -295,8 +335,11 @@ x++;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JSlider jSlider1;
